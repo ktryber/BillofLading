9ce1876daad5422f02dc2fb389b975d6cf53e032
Kris <ktryber@gmail.com>
2017-11-19 20:26:10 -0800

    fixing deploy

--- BillofLading/urls.py
+++ BillofLading/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import GeneratePDF
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('BolInput.urls')),
